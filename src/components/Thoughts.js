dba2ff94df6d1554b863304e648538a66298793a
Sonugc <Sonugc71@gmail.com>
2025-01-15 19:05:17 +0545

    links updated

--- src/components/Thoughts.js
+++ src/components/Thoughts.js
@@ -1,31 +1,46 @@
-import React from 'react'
-import './Thoughts.css'
+import React from 'react';
+import './Thoughts.css';
 
 function Thoughts() {
   return (
     <div className='thoughts-container'>
-        <p className='thoughts-top1'>PLACE YOUR THOUGHTS</p>
-        <p className='thoughts-top2'>We would love to hear feedbacks and questions from your end.</p>
-        <div className='thoughts-form'>
-            <div className='thoughts-input'>
-                <input type='fname' placeholder='First Name'/>
-            </div>
-            <div className='thoughts-input'>
-                <input type='lname' placeholder='Last Name'/>
-            </div>
-            <div className='thoughts-input'>
-                <input type='email' placeholder='Email Address'/>
-            </div>
-            <div className='thoughts-input'>
-                <input type='thoughts' className='thoughts-input-field' placeholder='Your Thoughts'/>
-            </div>
-            <div className='submit'>
-            <button className='thoughts-submit'>Submit</button>
-            </div>
+      <p className='thoughts-top1'>PLACE YOUR THOUGHTS</p>
+      <p className='thoughts-top2'>
+        We would love to hear feedbacks and questions from your end.
+      </p>
+      <div className='thoughts-form'>
+        <div className='thoughts-input'>
+          <input 
+            type='text'
+            placeholder='First Name'
+          />
         </div>
-      
+        <div className='thoughts-input'>
+          <input 
+            type='text'
+            placeholder='Last Name'
+          />
+        </div>
+        <div className='thoughts-input'>
+          <input 
+            type='email'
+            placeholder='Email Address'
+          />
+        </div>
+        <div className='thoughts-input'>
+          <textarea 
+            className='thoughts-input-field'
+            placeholder='Your Thoughts'
+          />
+        </div>
+        <div className='submit'>
+          <button className='thoughts-submit'>
+            Submit
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Thoughts
+export default Thoughts;
