acc21ac9e926316a27486660561e104a28658537
Sonugc <Sonugc71@gmail.com>
2025-01-27 20:54:06 +0545

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,40 @@
-import "./Navbar.css"
-import React from 'react'
-// import { Link } from 'react-router-dom'
-
+import React, { useState } from 'react';
+import { Menu, X } from 'lucide-react';
+import './Navbar.css';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
       <nav className="navbar">
-      <div className='navbar-container'>
-            <div  className="navbar-logo">
-                 <img src= '\Nutrition_logo333.png' alt ="Nutrition" className="logo-image" />
-            </div>
+        <div className='navbar-container'>
+          <div className="navbar-logo">
+            <img src='/Nutrition_logo333.png' alt="Nutrition" className="logo-image" />
+          </div>
 
+          {/* Hamburger menu button for mobile */}
+          <button className="mobile-menu-button" onClick={toggleMenu}>
+            {isOpen ? <X size={24} /> : <Menu size={24} />}
+          </button>
         </div>
 
-      <div className='navitems-container'>
-       
-        <ul className='nav-items'>
-          <a href='#aboutus'><li> About Us</li></a>
-          <a href='#events'> <li> Events</li></a>
-          <a href='#specialinitiatives'><li>Special Initiatives</li></a>
-          <a href='#shareyourthoughts'><li>Share Your Thoughts</li></a>
-          <a href='#contact'> <li> Contact Us</li></a>
-        </ul>
-      </div>
-
+        <div className={`navitems-container ${isOpen ? 'active' : ''}`}>
+          <ul className='nav-items'>
+            <a href='#aboutus'><li>About Us</li></a>
+            <a href='#events'><li>Events</li></a>
+            <a href='#specialinitiatives'><li>Special Initiatives</li></a>
+            <a href='#shareyourthoughts'><li>Share Your Thoughts</li></a>
+            <a href='#contact'><li>Contact Us</li></a>
+          </ul>
+        </div>
       </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
